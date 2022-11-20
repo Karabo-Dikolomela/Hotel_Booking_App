f4fff2fa9fd02dcba1d18aa1f014e27c2808c3d9
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-11-20 21:18:18 +0200

    fixed file structure
    Moved files into respective folders, created styles file

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <div id="background">
 
         <div class="title-container">
-            <img src="./css/images/8.png" alt="heavenly stay Logo" id="AppLogo">
+            <img src="./images/logo/8.png" alt="heavenly stay Logo" id="AppLogo">
             <p class="tagline">Book your heavenly stay location.
                 <button class="arrow">Login in to make a booking<a href="#booking-form"> <i class="arrow fas fa-level-up-alt"></i></a></button>
             </p>
@@ -35,7 +35,7 @@
 
             <!--CARD 1 CASTERLY ROCKL-->
             <div class="card">
-                <div class="card-image" style="background-image: url(/images/casterly-rock-1.jpg)"></div>
+                <div class="card-image" style="background-image: url(./images/hotels/cards/casterly-rock-1.jpg)"></div>
                 <div class="card-content">
 
                     <h1>Casterly Rock</h1>
@@ -68,53 +68,52 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
 
+            <!--CARD 2 BRAVOSI-->
+            <div class="card">
+                <div class="card-image" style="background-image: url(./images/hotels/cards/bravosi.jpg)"></div>
+                <div class="card-content">
 
-    <!--CARD 2 BRAVOSI-->
-    <div class="card">
-        <div class="card-image" style="background-image: url(/images/bravosi.jpg)"></div>
-        <div class="card-content">
+                    <h1>Bravosi</h1>
+                    <div class="subtitle">An atmospheric getaway</div>
+                    <p>
+                        Braavos, known as Braavos of the Hundred Isles, is the greatest and most powerful of the Free Cities,
+                        located in a lagoon on the northwestern end of Essos, where the narrow sea and the Shivering Sea meet.
+                        Bravosi, an endearing and humble hotel close to common beaches, stores and life in the hour of twilight.
+                        Bravosi is for the young and all those who wish to be engulfed and embraced by the city's charm.<br><br>
+                        <i class="fas fa-swimmer"></i> Pool: No<br>
+                        <i class="fas fa-wifi"></i> WiFi: Yes<br>
+                        <i class="fas fa-umbrella-beach"></i> Ocean view: Yes<br>
+                        <i class="fas fa-paw"></i> Pets allowed: Yes<br>
+                    </p>
 
-            <h1>Bravosi</h1>
-            <div class="subtitle">An atmospheric getaway</div>
-            <p>
-                Braavos, known as Braavos of the Hundred Isles, is the greatest and most powerful of the Free Cities,
-                located in a lagoon on the northwestern end of Essos, where the narrow sea and the Shivering Sea meet.
-                Bravosi, an endearing and humble hotel close to common beaches, stores and life in the hour of twilight.
-                Bravosi is for the young and all those who wish to be engulfed and embraced by the city's charm.<br><br>
-                <i class="fas fa-swimmer"></i> Pool: No<br>
-                <i class="fas fa-wifi"></i> WiFi: Yes<br>
-                <i class="fas fa-umbrella-beach"></i> Ocean view: Yes<br>
-                <i class="fas fa-paw"></i> Pets allowed: Yes<br>
-            </p>
-
-            <div class="card-details">
-                <div class="card-details-inner">
-                    <div class="read-more">
-                        <button class="button modal-btn-2">View Gallery</button>
-                    </div>
-                    <div class="reviews">
-                        <div>
-                            &#9733 &#9733 &#9733
-                        </div>
-                        <div>
-                            453 reviews
+                    <div class="card-details">
+                        <div class="card-details-inner">
+                            <div class="read-more">
+                                <button class="button modal-btn-2">View Gallery</button>
+                            </div>
+                            <div class="reviews">
+                                <div>
+                                    &#9733 &#9733 &#9733
+                                </div>
+                                <div>
+                                    453 reviews
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
-
     <!--HOTEL INFO SECTION 2-->
     <div class="card-container">
         <div class="flex-container">
 
             <!--CARD 3 HIGH GARDEN-->
             <div class="card">
-                <div class="card-image" style="background-image: url(images/high-garden.jpg)"></div>
+                <div class="card-image" style="background-image: url(./images/hotels/cards/high-garden.jpg)"></div>
                 <div class="card-content">
 
                     <h1>High Garden</h1>
@@ -149,118 +148,128 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <!--CARD 4 WINTERFELL-->
-    <div class="card">
-        <div class="card-image" style="background-image: url(images/winterfell.jpg)"></div>
-        <div class="card-content">
 
-            <h1>Winterfell</h1>
-            <div class="subtitle">All-in-one bliss</div>
-            <p>
-                Winterfell is the capital of the Kingdom of the North and the seat and the ancestral home of the royal House Stark.
-                It is a very large castle located at the center of the North, from where the head of House Stark rules over his or her people.
-                This Esteemed hotel offers a wide range of in-house royal entertainment, luxury spars and gourmet dishes.
-                Winterfell is for holiday makers who enjoy the simplicity of having everything in one place.
-                <br><br>
-                <i class="fas fa-swimmer"></i> Pool: Yes<br>
-                <i class="fas fa-wifi"></i> WiFi: Yes<br>
-                <i class="fas fa-umbrella-beach"></i> Ocean view: Yes<br>
-                <i class="fas fa-paw"></i> Pets allowed: No<br>
-            </p>
 
-            <div class="card-details">
-                <div class="card-details-inner">
-                    <div class="read-more">
-                        <button class="button modal-btn-4">View Gallery</button>
-                    </div>
-                    <div class="reviews">
-                        <div>
-                            &#9733 &#9733 &#9733
-                        </div>
-                        <div>
-                            421 reviews
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+            <!--CARD 4 WINTERFELL-->
+            <div class="card">
+                <div class="card-image" style="background-image: url(./images/hotels/cards/winterfell.jpg)"></div>
+                <div class="card-content">
 
-    <!--CARD 5 KINGS LANDING-->
-    <div class="card">
-        <div class="card-image" style="background-image: url(images/kings-landing.jpg)"></div>
-        <div class="card-content">
+                    <h1>Winterfell</h1>
+                    <div class="subtitle">All-in-one bliss</div>
+                    <p>
+                        Winterfell is the capital of the Kingdom of the North and the seat and the ancestral home of the royal House Stark.
+                        It is a very large castle located at the center of the North, from where the head of House Stark rules over his or her people.
+                        This Esteemed hotel offers a wide range of in-house royal entertainment, luxury spars and gourmet dishes.
+                        Winterfell is for holiday makers who enjoy the simplicity of having everything in one place.
+                        <br><br>
+                        <i class="fas fa-swimmer"></i> Pool: Yes<br>
+                        <i class="fas fa-wifi"></i> WiFi: Yes<br>
+                        <i class="fas fa-umbrella-beach"></i> Ocean view: Yes<br>
+                        <i class="fas fa-paw"></i> Pets allowed: No<br>
+                    </p>
 
-            <h1>King's Landing</h1>
-            <div class="subtitle">A home away from home</div>
-            <p class="hotel-info">
-                King's Landing is the largest city on the continent of Westeros and is the capital of the Seven Kingdoms.
-                It is located on the east coast of the continent, on the northern banks of the Blackwater Rush as it flows out into Blackwater Bay.
-                The city is not located within the borders of any of the former independent kingdoms.
-                The surrounding area are directly controlled from the city under the authority of the King on the Iron Throne.<br>
-                A distance from noisy streets, this hotel offers a piece of mind and a senctuary for those seeking an
-                escape from the stresses that come with daily endavours. Full privacy, full service. Bring your pets along if you wish.<br><br>
-                <i class="fas fa-swimmer"></i> Pool: Yes<br>
-                <i class="fas fa-wifi"></i> WiFi: Yes<br>
-                <i class="fas fa-umbrella-beach"></i> Ocean view: Yes<br>
-                <i class="fas fa-paw"></i> Pets allowed: Yes<br>
-            </p>
-
-            <div class="card-details">
-                <div class="card-details-inner">
-                    <div class="read-more">
-                        <button class="button modal-btn-5">View Gallery</button>
-                    </div>
-                    <div class="reviews">
-                        <div>
-                            &#9733 &#9733 &#9733 &#9733
-                        </div>
-                        <div>
-                            299 reviews
+                    <div class="card-details">
+                        <div class="card-details-inner">
+                            <div class="read-more">
+                                <button class="button modal-btn-4">View Gallery</button>
+                            </div>
+                            <div class="reviews">
+                                <div>
+                                    &#9733 &#9733 &#9733
+                                </div>
+                                <div>
+                                    421 reviews
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 
-    <!--CARD 6 THE VALE-->
-    <div class="card">
-        <div class="card-image" style="background-image: url(images/the-vale.jpg)"></div>
-        <div class="card-content">
+    <!--HOTEL INFO SECTION 3-->
+    <div class="card-container">
+        <div class="flex-container">
 
-            <h1>The Vale</h1>
-            <div class="subtitle">A fun-packed experience</div>
-            <p>
-                The Vale of Arryn is a fertile valley bordered by the Mountains of the Moon in eastern Westeros.
-                The valley gives its name to the larger realm ruled by House Arryn, commonly called the Vale of Arryn,
-                which includes the Mountains of the Moon, the Fingers, and islands in the Bite and the narrow sea.
-                Amidst a spectacular mountain range, this one's for the adventourous traveller.
-                Guests have a range of exhilarating activities to choose from, such as rockclimbing, ziplining and river rafting.<br><br>
-                <i class="fas fa-swimmer"></i> Pool: Yes<br>
-                <i class="fas fa-wifi"></i> WiFi: Yes<br>
-                <i class="fas fa-umbrella-beach"></i> Ocean view: Yes<br>
-                <i class="fas fa-paw"></i> Pets allowed: Yes<br>
-            </p>
+            <!--CARD 5 KINGS LANDING-->
+            <div class="card">
+                <div class="card-image" style="background-image: url(./images/hotels/cards/kings-landing.jpg)"></div>
+                <div class="card-content">
 
-            <div class="card-details">
-                <div class="card-details-inner">
-                    <div class="read-more">
-                        <button class="button modal-btn-6">View Gallery</button>
-                    </div>
-                    <div class="reviews">
-                        <div>
-                            &#9733 &#9733 &#9733
-                        </div>
-                        <div>
-                            453 reviews
+                    <h1>King's Landing</h1>
+                    <div class="subtitle">A home away from home</div>
+                    <p class="hotel-info">
+                        King's Landing is the largest city on the continent of Westeros and is the capital of the Seven Kingdoms.
+                        It is located on the east coast of the continent, on the northern banks of the Blackwater Rush as it flows out into Blackwater Bay.
+                        The city is not located within the borders of any of the former independent kingdoms.
+                        The surrounding area are directly controlled from the city under the authority of the King on the Iron Throne.<br>
+                        A distance from noisy streets, this hotel offers a piece of mind and a senctuary for those seeking an
+                        escape from the stresses that come with daily endavours. Full privacy, full service. Bring your pets along if you wish.<br><br>
+                        <i class="fas fa-swimmer"></i> Pool: Yes<br>
+                        <i class="fas fa-wifi"></i> WiFi: Yes<br>
+                        <i class="fas fa-umbrella-beach"></i> Ocean view: Yes<br>
+                        <i class="fas fa-paw"></i> Pets allowed: Yes<br>
+                    </p>
+
+                    <div class="card-details">
+                        <div class="card-details-inner">
+                            <div class="read-more">
+                                <button class="button modal-btn-5">View Gallery</button>
+                            </div>
+                            <div class="reviews">
+                                <div>
+                                    &#9733 &#9733 &#9733 &#9733
+                                </div>
+                                <div>
+                                    299 reviews
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <!--CARD 6 THE VALE-->
+            <div class="card">
+                <div class="card-image" style="background-image: url(images/the-vale.jpg)"></div>
+                <div class="card-content">
+
+                    <h1>The Vale</h1>
+                    <div class="subtitle">A fun-packed experience</div>
+                    <p>
+                        The Vale of Arryn is a fertile valley bordered by the Mountains of the Moon in eastern Westeros.
+                        The valley gives its name to the larger realm ruled by House Arryn, commonly called the Vale of Arryn,
+                        which includes the Mountains of the Moon, the Fingers, and islands in the Bite and the narrow sea.
+                        Amidst a spectacular mountain range, this one's for the adventourous traveller.
+                        Guests have a range of exhilarating activities to choose from, such as rockclimbing, ziplining and river rafting.<br><br>
+                        <i class="fas fa-swimmer"></i> Pool: Yes<br>
+                        <i class="fas fa-wifi"></i> WiFi: Yes<br>
+                        <i class="fas fa-umbrella-beach"></i> Ocean view: Yes<br>
+                        <i class="fas fa-paw"></i> Pets allowed: Yes<br>
+                    </p>
+
+                    <div class="card-details">
+                        <div class="card-details-inner">
+                            <div class="read-more">
+                                <button class="button modal-btn-6">View Gallery</button>
+                            </div>
+                            <div class="reviews">
+                                <div>
+                                    &#9733 &#9733 &#9733
+                                </div>
+                                <div>
+                                    453 reviews
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </body>
