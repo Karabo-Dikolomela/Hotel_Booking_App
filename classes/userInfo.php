9bf6c96cf27754bbf1a4207ef6e75f93d0b5d851
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-11-21 01:12:27 +0200

    Hotel Class
    Created hotel class object

---
+++ classes/userInfo.php
@@ -0,0 +1,27 @@
+<?php
+
+Class Hotel {
+    public $name;
+    public $description;
+    public $pool;
+    public $wifi;
+    public $ocean_view;
+    public $pets_allowed;
+    public $price;
+
+
+public function __construct($name, $description, $pool, $wifi ,$ocean_view, $pets_allowed, $price) {
+
+    $this-> name = $name;
+    $this-> description = $description;
+    $this-> pool = $pool;
+    $this-> wifi = $wifi;
+    $this-> ocean_view = $ocean_view;
+    $this-> pets_allowed = $pets_allowed;
+    $this-> price = $price;
+ }
+
+}
+
+$hotel1 = new Hotel ("Casterly Rock", "Upmarket hotel with a view of the ocean and private beach access.", "Yes", "Yes", "Yes", "No", 500 );
+$hotel1 = new Hotel ("Bravosi","Friendly and down-to-earth accommodation close to public beaches, shops and nightlife.", "No", "Yes", "Yes", "Yes", 350);
