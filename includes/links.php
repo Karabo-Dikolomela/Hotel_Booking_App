d141463229e397e12e36406a33738658056e9252
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-11-20 20:08:16 +0200

    header
    included logo and call to action message

---
+++ includes/links.php
@@ -0,0 +1,11 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css">
+    <link rel="icon" href="images/paradise-favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Arimo&family=Crimson+Text&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    include("./includes/links.php")
+    ?>
+    <title>Heavenly Stay</title>
+</head>
+
+<body>
+    <div id="background">
+
+        <div class="title-container">
+            <img src="./css/images/8.png" alt="heavenly stay Logo" id="AppLogo">
+            <p class="tagline">Book your heavenly stay location.
+                <button class="arrow">Login in to make a booking<a href="#booking-form"> <i class="arrow fas fa-level-up-alt"></i></a></button>
+            </p>
+        </div>
+    </div>
+
+    <!--CALL-TO-ACTION MESSAGE-->
+    <div id="call-to-action" class="call-to-action">
+        <h2 class="voyage-message">Tired from voyaging</h2>
+        <p>Make a stop at one of our luxurious hotels for a heavenly stay.<br><br>
+        </p>
+
+    </div>
+
+</body>
+
+</html>
