782dc3f515bc0d74602903f90624a11165888f73
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-11-25 17:50:37 +0200

    Connect to database
    successful

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,26 @@
 <?php
 
-$dbhost = 'localhost';
-$dbuser = 'Heavenly_Stay';
-$dbpass = 'heavenlySTAY@1 ';
-$dbname = 'registration';
+define('DB_NAME','registration');
+define('DB_USER','Heavenly_Stay');
+define('DB_PASSWORD','heavenlySTAY@1');
+define('DB_HOST','localhost');
 
-$connection = mysqli_connect($dbhost, $dbuser, $dbpass);
-if(!$connection ){
-    die('could not connect:' .mysqli_error());
+$connection = @mysqli_connect(DB_HOST,DB_USER, DB_PASSWORD, DB_NAME );
+
+if(!$connection){
+    die('Could not connect: '.mysqli_connect_error());
 }
-mysqli_select_db($connection,$dbname);
+
+// $dbhost = 'localhost';
+// $dbuser = 'Heavenly_Stay';
+// $dbpass = 'heavenlySTAY@1 ';
+// $dbname = 'registration';
+
+// $connection = mysqli_connect($dbhost, $dbuser, $dbpass);
+
+// if(!$connection ){
+//     die('could not connect:' .mysqli_error());
+// }
+// mysqli_select_db($connection,$dbname);
 
 ?>
